c8df18cf57f874473a99096d1d19b21c0ada485a
ongomobile <ongomobile@gmail.com>
2021-07-01 11:18:53 -0700

    Change font to Inter

--- src/components/common/Layout/styles.js
+++ src/components/common/Layout/styles.js
@@ -1,8 +1,8 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components"
 
 export const Global = createGlobalStyle`
   html {
-    font-family: 'Roboto', Helvetica, sans-serif;
+    font-family: 'Inter', Helvetica, sans-serif;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
 
@@ -571,7 +571,7 @@ export const Global = createGlobalStyle`
     code {
       background-color: hsla(0, 0%, 0%, 0.04);
       border-radius: 3px;
-      font-family: 'SFMono-Regular', Consolas, 'Roboto Mono', 'Droid Sans Mono',
+      font-family: 'SFMono-Regular', Consolas, 'Inter Mono', 'Droid Sans Mono',
         'Liberation Mono', Menlo, Courier, monospace;
       padding: 0;
       padding-top: 0.2em;
@@ -602,9 +602,9 @@ export const Global = createGlobalStyle`
     body {
       margin: 0;
       padding: 0;
-      font-family: 'Roboto', Helvetica, sans-serif;
-      color: ${({ theme }) => (theme === 'light' ? 'hsla(0, 0%, 0%, 0.8)' : '#fff')};
-      background-color: ${({ theme }) => (theme === 'light' ? '#fff' : '#212121')};
+      font-family: 'Inter', Helvetica, sans-serif;
+      color: ${({ theme }) => (theme === "light" ? "hsla(0, 0%, 0%, 0.8)" : "#fff")};
+      background-color: ${({ theme }) => (theme === "light" ? "#fff" : "#212121")};
       transition: .3s all;
       font-weight: normal;
       word-wrap: break-word;
@@ -625,4 +625,4 @@ export const Global = createGlobalStyle`
       }
     }
   }
-`;
+`
