57102b3e8adc9a37741737204786fb1fec2b37a5
ongomobile <ongomobile@gmail.com>
2021-07-01 10:16:20 -0700

    Rename files, refactor design

--- src/components/landing/Projects/index.js
+++ src/components/landing/Projects/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Container } from "components/common"
+import { Wrapper } from "./styles"
 
 export const Projects = () => {
   return (
-    <div>
+    <Wrapper as={Container} id="work">
       <h1>Projects</h1>
-    </div>
+    </Wrapper>
   )
 }
