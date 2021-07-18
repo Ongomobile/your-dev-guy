4c9106def7ac0c7df47c44b6d582cf15acf0abec
ongomobile <ongomobile@gmail.com>
2021-07-17 19:24:16 -0700

    Update styles

--- src/components/landing/About/styles.js
+++ src/components/landing/About/styles.js
@@ -9,6 +9,7 @@ export const AboutHeadline = styled.h2`
   font-size: 35px;
   font-weight: 600;
   color: rgba(42, 48, 55, 0.8);
+  margin-bottom: 80px;
 
   @media (max-width: 540px) {
     font-size: 25px;
@@ -21,19 +22,18 @@ export const Subheadline = styled.h3`
 `
 
 export const ImageWrapper = styled.div`
-  margin-bottom: 40px;
+  margin-bottom: 20px;
 `
 
 export const CardsWrapper = styled.div`
+  margin-left: -20px;
   display: flex;
   justify-content: space-between;
-  margin-top: 40px;
 
   @media (max-width: 1280px) {
     flex-direction: column;
     max-width: 680px;
+    margin-left: 0;
   }
 `
-export const BtnWrapper = styled.div`
-  margin-left: 18px;
-`
+export const BtnWrapper = styled.div``
