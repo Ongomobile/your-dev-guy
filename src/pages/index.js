fe8b9beefad3c98deff1be46fb2df9a5cff4fd65
ongomobile <ongomobile@gmail.com>
2021-10-25 14:09:01 -0700

    Add cookie consent component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 import { Layout, Seo } from "components/common"
 import { Intro, Skills, Contact, Projects } from "components/landing"
 import { About } from "components/landing/About"
+import CookieConsent from "react-cookie-consent"
 
 const Home = () => (
   <Layout>
@@ -13,6 +15,30 @@ const Home = () => (
       <About />
       <Contact />
     </main>
+    <CookieConsent
+      location="bottom"
+      buttonText="Ok"
+      cookieName="myAwesomeCookieName2"
+      style={{ background: "#2B373B", paddingRight: "2%" }}
+      buttonStyle={{
+        color: "#4e503b",
+        backgroundColor: "#F9B233",
+        fontSize: "13px",
+        width: "80px",
+        borderRadius: "3px",
+      }}
+      acceptOnScroll={true}
+      acceptOnScrollPercentage={50}
+      expires={150}
+      debug={true}
+    >
+      By continuing to browse or by clicking 'OK', you agree to the storing of cookies on your device to enhance your
+      site experience and for analytical purposes. To learn more see{" "}
+      <Link to="/" activeStyle={{ color: "#57a0ff" }}>
+        cookies policy
+      </Link>
+      .
+    </CookieConsent>
   </Layout>
 )
 
